0717f31f18f09acdc43a4c869f40c146c4d53de9
jrlo35 <jonathon.lo35@gmail.com>
2015-09-23 00:32:15 -0700

    Update Blackjack.js

--- Blackjack.js
+++ Blackjack.js
@@ -9,16 +9,14 @@ function getValue(card) {
     if(card%13===0|| card%13>10) {
         return 10;
     }
-    
     else if(card%13===1) {
         return 'Ace';
     }
     else {
-    
     return card%13;
     }
 }
-//hit
+//generate next card
 function nextcard() {
     return getValue(deal());
 }
@@ -34,7 +32,7 @@ function isBlackjack(hand) {
 function printDealer(compHand) {
     console.log("Dealer showing " + compHand[0]);//show first card
 }
-
+//format print statement
 function formatHand(hand) {
     var upper = calcUpper(hand);
     var lower = calcLower(hand);
@@ -51,6 +49,7 @@ function formatHand(hand) {
 function compChoice(compHand) {
     var bool=true;
     while(bool){
+        //deal next card
         console.log("Dealer's Hand: " + formatHand(compHand));
         if(calcUpper(compHand)<17 || calcLower(compHand) == 7) {
             //hit on soft 17
@@ -103,27 +102,29 @@ function reduce(collection,predicate,start) {
     })
     return start;
 }
-
-function calcUpper(collection) {
+//calculate total value of hands
+function calcUpper(hand) {
     var aceCount = 0;
     var sum = 0;
-    for (var i = 0; i < collection.length; i++) {
-        if (collection[i] == 'Ace') {
+    //assume all Aces are worth 11
+    for (var i = 0; i < hand.length; i++) {
+        if (hand[i] == 'Ace') {
             aceCount++;
             sum += 11;
         } else {
-            sum += collection[i];
+            sum += hand[i];
         }
     }
+    //subtract 10 for each ace until sum less than 21
     while (sum > 21 && aceCount > 0) {
         sum -= 10;
         aceCount--;
     }
     return sum;
 }
-
-function calcLower(collection) {
-    return reduce(collection, function(curr, total) {
+//all Aces equals 1, calculate lowest possible value
+function calcLower(hand) {
+    return reduce(hand, function(curr, total) {
         if (curr == 'Ace') {
             return total + 1;
         } else {
@@ -139,60 +140,60 @@ function main() {
     var mycard2=nextcard();
     var compcard2= nextcard();
    
-    var myHand=[myCard1,mycard2];
+    var myHand=[myCard1,mycard2];//your hand
     var compHand=[compcard1,compcard2];//dealer hand
     console.log("Your hand: " + formatHand(myHand));
-    printDealer(compHand);
-    
+    printDealer(compHand);//show dealers first card
+    //both players blackjack
     if(isBlackjack(compHand)&&isBlackjack(myHand)){
         console.log("Dealer's Hand: " + formatHand(compHand));
         console.log("Your hand: " + formatHand(myHand));
         console.log('Push.')
         return;
     }
-    
+    //check computer blackjack
     if (isBlackjack(compHand)) {
         formatHand(compHand);
         console.log('\nBlackjack. Dealer wins.');
         return;
     }
-    
+    //check player blackjack
     if(isBlackjack(myHand)){
         formatHand(myHand);
         console.log('\nBlackjack. You win.');
         return;
     }
-    
+    //call player choice
     var choice= playerChoice(myHand);
     if(choice===false){
         return;
     }
-    
+    //call dealer choice
     compChoice(compHand);
     if(calcUpper(myHand)===calcUpper(compHand)){
         console.log('\nPush.');
         return;
     }
+    //determine winner
     if(calcUpper(myHand)>calcUpper(compHand)&& calcUpper(myHand)<=21){
         console.log('You Win!');
         return;
         
     }
-    
     else if(calcUpper(compHand)<=21&& calcUpper(compHand)>calcUpper(myHand)){
         console.log('You Lose.');
         return;
     }
-    
 }
 
 main();
 
-
+//player logic
 function playerChoice(collection) {
     var bool=true;
     
     while(bool) {
+        //hit 
         var ask=prompt('Type: hit or stay');
         if(ask==='hit'){
             collection.push(nextcard());
@@ -206,14 +207,8 @@ function playerChoice(collection) {
                 return false;
                 
             }
-        
-        }
-    
-        
+        }//stay
         else{
-            
-            
-            
             break;
         }
     }
